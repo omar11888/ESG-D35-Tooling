d231a76c530ea58754c64b991d6fd251198bd43b
Omar Mohamed <amooory1888@gmail.com>
2022-08-29 20:57:46 +0200

    Fixing code issue

--- func.c
+++ func.c
@@ -3,14 +3,14 @@
 
 void print_my_name(void)
 {
-	printf("My name is Omar\n");
+	printf("My name is Omar Mohamed\n");
 	
 	return 0;
 }
 
 void print_my_age(void)
 {
-	printf("My age is 22\n");
+	printf("My age is 23\n");
 	
 	return 0;
 }
